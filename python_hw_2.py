cf66edc16e8dc5969c90355c7ee636ae10fa9db0
Bryazgin Sergey <sergeybryazgin@gmail.com>
2023-10-10 23:34:11 +0300

    на проверку

--- python_hw_2.py
+++ python_hw_2.py
@@ -91,15 +91,23 @@ def test_readable_function():
 
 
 def open_browser(browser_name):
-    actual_result = None
+    actual_result = make_readable(open_browser, browser_name)
     assert actual_result == "Open Browser [Chrome]"
 
 
 def go_to_companyname_homepage(page_url):
-    actual_result = None
+    actual_result = make_readable(go_to_companyname_homepage, page_url)
     assert actual_result == "Go To Companyname Homepage [https://companyname.com]"
 
 
 def find_registration_button_on_login_page(page_url, button_text):
-    actual_result = None
+    actual_result = make_readable(find_registration_button_on_login_page, page_url,button_text)
     assert actual_result == "Find Registration Button On Login Page [https://companyname.com/login, Register]"
+
+
+def make_readable(func, *args):
+    name = func.__name__.replace('_',' ').title()
+    result = f"{name} [{', '.join(args)}]"
+    print(result)
+    return result
+
