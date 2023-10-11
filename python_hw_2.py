5d3598a8396ec83e76610d15f516fa7ced7fdbe2
Bryazgin Sergey <sergeybryazgin@gmail.com>
2023-10-11 09:05:01 +0300

    внес правки

--- python_hw_2.py
+++ python_hw_2.py
@@ -8,7 +8,6 @@ def test_dark_theme_by_time():
     current_time = time(hour=23)
     # TODO переключите темную тему в зависимости от времени суток (с 22 до 6 часов утра - ночь)
 
-    is_dark_theme = None
     if time(hour=6) <= current_time <= time(hour=22):
         is_dark_theme = False
     else:
@@ -29,7 +28,6 @@ def test_dark_theme_by_time_and_user_choice():
     # TODO переключите темную тему в зависимости от времени суток,
     #  но учтите что темная тема может быть включена вручную
 
-    is_dark_theme = None
     if dark_theme_enabled_by_user == True:
         is_dark_theme = True
     elif dark_theme_enabled_by_user == False:
@@ -62,7 +60,6 @@ def test_find_suitable_user():
     assert suitable_users == {"name": "Olga", "age": 45}
 
     # TODO найдите всех пользователей младше 20 лет
-    suitable_users = None
     suitable_users = []
     for user in users:
         if user["age"] < 20:
